c566825e8a470bd979ff5dc6038b1d358933106f
TomoyaNishi <nishi.tomoya.519@gmail.com>
2021-06-26 17:52:50 +0900

    typo修正

--- src/storage.spec.ts
+++ src/storage.spec.ts
@@ -3,8 +3,8 @@ import { Cookie, setCookie } from './storage';
 /*
     specification
         getter
-            - [] 1-1. if access proptery is in cookie, returns the value
-            - [] 1-2. if access proptery is not in cookie, returns null.
+            - [] 1-1. if access property is in cookie, returns the value
+            - [] 1-2. if access property is not in cookie, returns null.
         setter
             - [] 1-1. 何かを入力したときは
             - [] 1-2. 何も入力しなかったとき
